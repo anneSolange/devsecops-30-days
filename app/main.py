19fa18b8950ecb79c9e263dad3c9714ea05085cc
Lise Eloundou <annesolangeeloundou@gmail.com>
2025-07-14 11:13:36 +0200

    envoir

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
+# 1. Page HTML (Frontend)
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    content = templates.get_template("index.html").render(request=request)
+    response = HTMLResponse(content=content)
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# 2. API JSON (Backend)
+@app.get("/api")
+def api_home():
     return {"message": "Hello DevSecOps"}
-
-
